b31a4fd3c10443d8e7d19fd2afd8dd3c121dd8c9
cangsulan <3024179062@qq.com>
2024-04-27 21:27:37 +0800

    111

--- Diary.cpp
+++ Diary.cpp
@@ -25,10 +25,10 @@ Diary::~Diary() {
 void Diary::showDiary() {
 	this->views++;
 	cout << "日记编号：" << this->id << endl;
-	cout << "作者名：" << this->stuName << endl;
-	cout << "作者学生id：" << this->stuId << endl;
+	cout << "作者名：" << this->stuName;
+	cout << "\t\t作者学生id：" << this->stuId << endl;
 	cout << "浏览量：" << this->views;
-	cout << "\t\t平均评分：" << this->rating << endl;
+	printf("\t\t平均评分：%.2f\n", this->rating);//保留2位小数打印！
 	cout << "以下为 日记内容：" << endl;
 	cout << this->content << endl<<endl;
 	while (true) {
@@ -39,8 +39,15 @@ void Diary::showDiary() {
 		cin >> select;
 		if (select == 1) {
 			cout << "请输入 0-10 内的一个整数来 打分：" << endl;
-			int r;
-			cin >> r;
+			int r=5;//同初始默认评分
+			while (true) {
+				cin >> r;
+				if (r >= 0 && r <= 10) {
+					break;
+				}else {
+					cout << "输入有误哦~" << endl;
+				}
+			}
 			this->rating = this->rating * this->ratedSize + r;
 			this->ratedSize++;
 			this->rating = this->rating / this->ratedSize;//得到了平均评分
@@ -54,9 +61,9 @@ void Diary::showDiarylog()
 {
 	cout << endl;
 	cout << "日记编号：" << this->id << endl;
-	cout << "作者名：" << this->stuName << endl;
+	cout << "作者名：" << this->stuName;
 	cout << "\t\t作者学生id：" << this->stuId << endl;
 	cout << "浏览量：" << this->views;
-	cout << "\t\t平均评分：" << this->rating << endl;
+	printf("\t\t平均评分：%.2f\n", this->rating);//保留2位小数打印！
 	cout << "---------------------------------" << endl;
 }
