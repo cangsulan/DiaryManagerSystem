f5901e166e31f151502445da6e064b72df101158
cangsulan <3024179062@qq.com>
2024-04-28 12:11:55 +0800

    还有全文检索和压缩存储没实现

--- HeapSort.hpp
+++ HeapSort.hpp
@@ -7,4 +7,6 @@
 void heapify(vector<Diary*>& heap, int n, int i);
 void buildHeap(vector<Diary*>& heap);
 void insertToMinHeap(vector<Diary*>& heap, Diary* diary);
+
+//日记堆排序前10位，select可选择排序依据，1表示热度，2表示平均评分
 void heapsortEnter(vector<Diary*> placeRelevant,int select);
